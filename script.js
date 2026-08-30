cdcf3cc5d38a5e973b05e4aa8da37603005073b2
algeriadigital <mitoadouane@gmail.com>
2026-08-30 15:57:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+let stars = 0;
+let selectedCharacter = "👧 مريم";
 
+function showScreen(id) {
+  document.querySelectorAll(".screen").forEach(screen => {
+    screen.classList.remove("active");
+  });
+
+  document.getElementById(id).classList.add("active");
+}
+
+function startGame() {
+  showScreen("characters");
+}
+
+function selectCharacter(name) {
+  selectedCharacter = name;
+
+  document.getElementById("selectedCharacter").textContent =
+    "شخصيتك: " + name;
+
+  showScreen("worlds");
+}
+
+function selectWorld(world) {
+  document.getElementById("selectedWorld").textContent =
+    world;
+
+  showScreen("levels");
+}
+
+function openLevel(level) {
+
+  document.getElementById("selectedLevel").textContent =
+    "المرحلة " + level;
+
+  showScreen("coloring");
+
+}
+
+function completeColoring() {
+
+  stars += 10;
+
+  document.getElementById("stars").textContent =
+    "⭐ " + stars;
+
+  document.getElementById("rewardStars").textContent =
+    "+10 نجوم";
+
+  showScreen("reward");
+
+}
+
+function listenDua() {
+
+  alert(
+    "🤲 دعاء جميل للأطفال\\n\\nبسم الله"
+  );
+
+}
